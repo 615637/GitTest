c1022a382e1b345ebc7c58c8d2164ddd605015c8
615637 <129474590+615637@users.noreply.github.com>
2023-03-31 15:20:18 +0900

    자바 수업자료 업로드

---
+++ WorkSpace/01.HelloWorld/src/LYJ.java
@@ -0,0 +1,8 @@
+//어떤 설명을 내가 써놓고 싶을 때 어떻게 했을까?
+public class LYJ {
+public static void main(String[] args) {
+	
+	System.out.println("LYJ");
+	
+}
+}
---
+++ WorkSpace/999.Test/src/Test04.java
@@ -0,0 +1,22 @@
+
+public class Test04 {
+	public static void main(String[] args) {
+		int iNum;
+		double dNum;
+		char cChr;
+		String sStr;
+		
+		//변수의 값 : 리터럴
+		iNum=10;
+		dNum=2.5;
+		cChr='A';
+		sStr="이름";
+		
+		//나열 : 같은 타입의 변수를 여러개 동시에 선언
+		int subject1, subject2, subject3;
+		subject1=1;
+		subject2=2;
+		subject3=3;
+		System.out.println(subject1);
+	}
+}
